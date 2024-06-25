e40eaa18f9ec04514c7907a3ed99cc38814798cb
muteshev <122206970+muteshev@users.noreply.github.com>
2024-06-24 20:11:22 -0700

    commit 7

--- catalog-service/src/main/java/dev/zenfluxpro/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/dev/zenfluxpro/bookstore/catalog/CatalogServiceApplication.java
@@ -3,6 +3,7 @@ package dev.zenfluxpro.bookstore.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Catalog Service
 @SpringBootApplication
 public class CatalogServiceApplication {
 
