f48ee8007e1d2989ec62e98306e93e38c2d6387b
muteshev <122206970+muteshev@users.noreply.github.com>
2024-06-26 21:56:57 -0700

    dockerhub

--- catalog-service/src/main/java/dev/zenfluxpro/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/dev/zenfluxpro/bookstore/catalog/CatalogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-
+//
 @SpringBootApplication
 // @ConfigurationPropertiesScan
 @EnableConfigurationProperties(ConfigProperties.class)
